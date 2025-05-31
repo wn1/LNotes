75f937efda192b2564579b731e78c8270d9041be
Kudashov Vladimir <v.kudashov.83@yandex.ru>
2025-05-31 21:56:16 +0300

    Обновление NotesHomeActivity

---
+++ app/src/main/java/ru/qdev/lnotes/core/db/DbManager.kt
@@ -0,0 +1,19 @@
+package ru.qdev.lnotes.core.events
+
+import android.content.Context
+import androidx.annotation.AnyThread
+import dagger.hilt.android.qualifiers.ApplicationContext
+import org.greenrobot.eventbus.EventBus
+import ru.qdev.lnotes.mvp.QDVMvpDbPresenter
+import ru.qdev.lnotes.mvp.QDVNavigationDrawerState
+
+class DbManager (@ApplicationContext val context: Context) {
+
+    @AnyThread
+    fun doReloadDb() {
+        QDVNavigationDrawerState().selectedFolderOrMenu = null
+        EventBus.getDefault().post(QDVMvpDbPresenter.DoCloseDatabase())
+        EventBus.getDefault().post(QDVMvpDbPresenter.DoReloadDatabase())
+    }
+
+}
