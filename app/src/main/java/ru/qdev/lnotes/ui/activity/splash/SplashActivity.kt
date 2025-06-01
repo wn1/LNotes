cdf3ac9ded8345df48795d288c31491d639e279f
Kudashov Vladimir <v.kudashov.83@yandex.ru>
2025-06-01 19:40:15 +0300

    Добавление темы Splash screen с возможностью db backup и restore

---
+++ app/src/main/java/ru/qdev/lnotes/ui/activity/splash/SplashActivity.kt
@@ -0,0 +1,120 @@
+package ru.qdev.lnotes.ui.activity.splash
+
+import android.content.Intent
+import android.content.res.Configuration
+import android.os.Bundle
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.navigationBarsPadding
+import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.zIndex
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.lifecycle.lifecycleScope
+import com.example.reply.ui.theme.AppTheme
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import ru.qdev.lnotes.ui.activity.QDVBackupActivity
+import ru.qdev.lnotes.ui.activity.notes.QDVNotesHomeActivity
+import ru.qdev.lnotes.ui.navigation.route.note.NoteListRoute
+import src.R
+
+class SplashActivity : AppCompatActivity() {
+    private var launchTimerJob: Job? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContent {
+            AppTheme {
+                ActivityContent()
+            }
+        }
+    }
+
+    override fun onResume() {
+        super.onResume()
+
+        startLaunchTimer()
+    }
+
+    @Composable
+    private fun ActivityContent(){
+        Box (modifier = Modifier.fillMaxSize()) {
+            Image(
+                modifier = Modifier.align(Alignment.Center),
+                painter = painterResource(R.drawable.logo_notes_app_144dp),
+                contentDescription = stringResource(R.string.app_name)
+            )
+
+            Button(
+                modifier = Modifier.align(Alignment.BottomCenter).zIndex(10f),
+                onClick = {
+                    startBackupActivity()
+                }
+            ) {
+                Text(text = stringResource(R.string.action_backup_restore))
+            }
+        }
+    }
+
+    private fun startBackupActivity() {
+        stopLaunchTimer()
+        val sendDataActivityStartIntent = Intent(
+            this,
+            QDVBackupActivity::class.java
+        )
+        sendDataActivityStartIntent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
+        startActivity(sendDataActivityStartIntent)
+    }
+
+    private fun startNotesActivity() {
+        val sendDataActivityStartIntent = Intent(
+            this,
+            QDVNotesHomeActivity::class.java
+        )
+        sendDataActivityStartIntent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
+        startActivity(sendDataActivityStartIntent)
+    }
+
+    private fun startLaunchTimer() {
+        launchTimerJob?.cancel()
+        launchTimerJob = lifecycleScope.launch {
+            delay(3000)
+            startNotesActivity()
+            finish()
+        }
+    }
+
+    private fun stopLaunchTimer() {
+        launchTimerJob?.cancel()
+    }
+
+    @Composable
+    @Preview(
+        uiMode = Configuration.UI_MODE_NIGHT_YES,
+        name = "DefaultPreviewDark"
+    )
+    @Preview(
+        uiMode = Configuration.UI_MODE_NIGHT_NO,
+        name = "DefaultPreviewLight"
+    )
+    private fun ActivityContentPreview() {
+        ActivityContent()
+    }
+}
+
