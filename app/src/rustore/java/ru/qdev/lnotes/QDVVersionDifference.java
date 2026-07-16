d8962b56feac91189f6b5fb9870a2d0add76e6a1
Vladimir Kudashov <v.kudashov.work@mail.ru>
2026-07-16 07:47:14 +0300

    Добавление варианта сборки

---
+++ app/src/rustore/java/ru/qdev/lnotes/QDVVersionDifference.java
@@ -0,0 +1,20 @@
+package ru.qdev.lnotes;
+
+/**
+ * Created by Vladimir Kudashov on 31.03.18.
+ */
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import org.jetbrains.annotations.Nullable;
+
+public class QDVVersionDifference {
+    static public boolean isFreeVersion() { return false; }
+
+    @Nullable
+    static public TextView getLabelBuyPlusVersion (ViewGroup rootLayout) {
+        return null;
+    }
+}
